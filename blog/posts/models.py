9df6a5d4288c7ec9fe6b020f2fd737be770a6659
ea.sobyanin2018@gmail.com <PC1013487026@DTDF.ru>
2025-10-13 11:17:34 +0300

    Задание 2.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from .constants import MAX_LENGTH_POST
+
+
+class Post(models.Model):
+    """Модель публикации."""
+
+    title = models.CharField(
+        'Заголовк',
+        max_length=MAX_LENGTH_POST,
+        db_index=True
+    )
+    text = models.TextField('Текст поста')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
+        default_related_name = 'posts'
+
+    def __str__(self):
+        return self.title
